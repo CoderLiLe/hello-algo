5f6776838fba81873620d528704338a8498728cc
1250124009@qq.com <1250124009@qq.com>
2020-01-05 22:28:10 +0800

    feat：最长公共子串优化

--- 04-Java/08-动态规划/src/com/lile/LCSubstring.java
+++ 04-Java/08-动态规划/src/com/lile/LCSubstring.java
@@ -22,6 +22,34 @@ public class LCSubstring {
 		int[] dp = new int[colChars.length + 1];
 		int max = 0;
 		for (int row = 1; row < rowChars.length; row++) {
+			for (int col = colChars.length; col >= 1; col--) {
+				if (rowChars[row - 1] != colChars[col - 1]) {
+					dp[col] = 0;
+				} else {
+					dp[col] = dp[col - 1] + 1;
+					max = Math.max(dp[col], max);
+				}
+			}
+		}
+		return max;
+	}
+	
+	static int lcs2(String str1, String str2) {
+		if (str1 == null || str2 == null) return 0;
+		char[] chars1 = str1.toCharArray();
+		if (chars1.length == 0) return 0;
+		char[] chars2 = str2.toCharArray();
+		if (chars2.length == 0) return 0;
+		
+		char[] rowChars = chars1, colChars = chars2;
+		if (chars1.length < chars2.length) {
+			rowChars = chars2;
+			colChars = chars1;
+		}
+		
+		int[] dp = new int[colChars.length + 1];
+		int max = 0;
+		for (int row = 1; row < rowChars.length; row++) {
 			for (int col = 1; col < colChars.length; col++) {
 				if (rowChars[row - 1] != colChars[col - 1]) continue;
 				dp[col] = dp[col - 1] + 1;
