e74151a1241e589115bf706084f4ea029cdff942
1250124009@qq.com <1250124009@qq.com>
2020-01-07 11:48:38 +0800

    feat：字符串匹配-KMP算法

--- 04-Java/11-字符串/src/com/lile/Main.java
+++ 04-Java/11-字符串/src/com/lile/Main.java
@@ -5,10 +5,10 @@ import com.lile.tool.Asserts;
 public class Main {
 
 	public static void main(String[] args) {
-		Asserts.test(BruteForce2.indexOf("Hello World", "H") == 0);
-		Asserts.test(BruteForce2.indexOf("Hello World", "d") == 10);
-		Asserts.test(BruteForce2.indexOf("Hello World", "or") == 7);
-		Asserts.test(BruteForce2.indexOf("Hello World", "abc") == -1);
+		Asserts.test(KMP.indexOf("Hello World", "H") == 0);
+		Asserts.test(KMP.indexOf("Hello World", "d") == 10);
+		Asserts.test(KMP.indexOf("Hello World", "or") == 7);
+		Asserts.test(KMP.indexOf("Hello World", "abc") == -1);
 	}
 
 }
