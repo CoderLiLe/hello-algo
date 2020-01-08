c4e7390c192047384f8b22e1ec5738166795199c
LiLe <1250124009@qq.com>
2020-01-08 11:23:33 +0800

    feat：图的深度优先搜索-DFS

--- 04-Java/03-图/src/com/lile/Main.java
+++ 04-Java/03-图/src/com/lile/Main.java
@@ -6,6 +6,11 @@ import com.lile.graph.ListGraph;
 public class Main {
 
 	public static void main(String[] args) {
+//		testBFS();
+		testDFS();
+	}
+	
+	static void test() {
 		Graph<String, Integer> graph = new ListGraph<>();
 		graph.addEdge("V1", "V0", 9);
 		graph.addEdge("V1", "V2", 3);
@@ -20,6 +25,56 @@ public class Main {
 //		((ListGraph<String, Integer>)graph).print();
 		
 		graph.bfs("V1");
+	}
+	
+	static void testBFS() {
+		Graph<Object, Double> graph = directedGraph(Data.BFS_02);
+		graph.bfs(7);
+	}
+	
+	static void testDFS() {
+		Graph<Object, Double> graph = directedGraph(Data.DFS_01);
+		graph.dfs(2);
+	}
+	
+	/**
+	 * 有向图
+	 */
+	private static Graph<Object, Double> directedGraph(Object[][] data) {
+		Graph<Object, Double> graph = new ListGraph<>();
+		for (Object[] edge : data) {
+			if (edge.length == 1) {
+				graph.addVertex(edge[0]);
+			} else if (edge.length == 2) {
+				graph.addEdge(edge[0], edge[1]);
+			} else if (edge.length == 3) {
+				double weight = Double.parseDouble(edge[2].toString());
+				graph.addEdge(edge[0], edge[1], weight);
+			}
+		}
+		return graph;
+	}
+	
+	/**
+	 * 无向图
+	 * @param data
+	 * @return
+	 */
+	private static Graph<Object, Double> undirectedGraph(Object[][] data) {
+		Graph<Object, Double> graph = new ListGraph<>();
+		for (Object[] edge : data) {
+			if (edge.length == 1) {
+				graph.addVertex(edge[0]);
+			} else if (edge.length == 2) {
+				graph.addEdge(edge[0], edge[1]);
+				graph.addEdge(edge[1], edge[0]);
+			} else if (edge.length == 3) {
+				double weight = Double.parseDouble(edge[2].toString());
+				graph.addEdge(edge[0], edge[1], weight);
+				graph.addEdge(edge[1], edge[0], weight);
+			}
+		}
+		return graph;
 	}
 
 }
