deef5068b49bffdf2948794c42075ae6c8f65af5
LiLe <1250124009@qq.com>
2020-02-02 19:58:18 +0800

    feat：集合

---
+++ 04-Java/09-集合/src/com/lile/tree/BinaryTree.java
@@ -0,0 +1,241 @@
+package com.lile.tree;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@SuppressWarnings("unchecked")
+public class BinaryTree<E> {
+	protected int size;
+	protected Node<E> root;
+	
+	public int size() {
+		return size;
+	}
+	
+	public boolean isEmpty() {
+		return size == 0;
+	}
+	 
+	public void clear() {
+		root = null;
+		size = 0;
+	}
+	
+	public void preOrder(Visitor<E> visitor) {
+		if (visitor == null) return;
+		preOrder(root, visitor);
+	}
+	
+	public void inOrder(Visitor<E> visitor) {
+		if (visitor == null) return;
+		inOrder(root, visitor);
+	}
+
+	public void postOrder(Visitor<E> visitor) {
+		if (visitor == null) return;
+		postOrder(root, visitor);
+	}
+	
+	public void levelOrder(Visitor<E> visitor) {
+		if (root == null || visitor == null) return;
+		
+		Queue<Node<E>> queue = new LinkedList<>();
+		queue.offer(root);
+		
+		while (!queue.isEmpty()) {
+			Node<E> node = queue.poll();
+			if (visitor.visit(node.element)) return;
+			
+			if (node.left != null) {
+				queue.offer(node.left);
+			}
+			
+			if (node.right != null) {
+				queue.offer(node.right);
+			}
+		}
+	}
+	
+	private void preOrder(Node<E> node, Visitor<E> visitor) {
+		if (node == null || visitor.stop) return;
+		
+		visitor.stop = visitor.visit(node.element);
+		preOrder(node.left, visitor);
+		preOrder(node.right, visitor);
+	}
+	
+	private void inOrder(Node<E> node, Visitor<E> visitor) {
+		if (node == null || visitor.stop) return;
+		
+		inOrder(node.left, visitor);
+		if (visitor.stop) return;
+		visitor.stop = visitor.visit(node.element);
+		inOrder(node.right, visitor);
+	}
+
+	private void postOrder(Node<E> node, Visitor<E> visitor) {
+		if (node == null || visitor.stop) return;
+		
+		postOrder(node.left, visitor);
+		postOrder(node.right, visitor);
+		if (visitor.stop) return;
+		visitor.stop = visitor.visit(node.element);
+	}
+	
+	/**
+	 * 判断改二叉树是否是完全二叉树
+	 */
+	public boolean isComplete() {
+		if (root == null) return false;
+		Queue<Node<E>> queue = new LinkedList<>();
+		queue.offer(root);
+		
+		boolean leaf = false;
+		while (!queue.isEmpty()) {
+			Node<E> node = queue.poll();
+			if (leaf && !node.isLeaf()) return false;
+			
+			if (node.left != null) {
+				queue.offer(node.left);
+			} else if (node.right != null) { // 一个节点没有左子树，但是有右子树，则肯定不是完全二叉树
+				return false;
+			}
+			
+			if (node.right != null) {
+				queue.offer(node.right);
+			} else { // 后面遍历的都必须是叶子节点
+				leaf = true;
+			}
+		}
+		
+		return true;
+	}
+	
+	public int height() {
+		if (root == null) return 0;
+		
+		// 树的高度
+		int height = 0;
+		// 存储每一层的元素数量
+		int levelSize = 1;
+		Queue<Node<E>> queue = new LinkedList<>();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			Node<E> node = queue.poll();
+			levelSize--;
+			
+			if (node.left != null) {
+				queue.offer(node.left);
+			}
+			
+			if (node.right != null) {
+				queue.offer(node.right);
+			}
+			
+			if (levelSize == 0) {
+				levelSize = queue.size();
+				height++;
+			}
+		}
+		return height;
+	}
+	
+	public int heightRecursion() {
+		return heightRecursion(root);
+	}
+	
+	private int heightRecursion(Node<E> node) {
+		if (node == null) return 0;
+		return 1 + Math.max(heightRecursion(node.left), heightRecursion(node.right));
+	}
+	
+	protected Node<E> createNode(E element, Node<E> parent) {
+		return new Node<>(element, parent);
+	}
+	
+	protected Node<E> predecessor(Node<E> node) {
+		if (node == null) return null;
+		
+		// 前驱节点在左子树当中（left.right.right.right....）
+		Node<E> p = node.left;
+		if (p != null) {
+			while (p.right != null) {
+				p = p.right;
+			}
+			return p;
+		}
+		
+		// 从父节点、祖父节点中寻找前驱节点
+		while (node.parent != null && node.parent.left == node) {
+			node = node.parent;
+		}
+		
+		return node.parent;
+	}
+	
+	protected Node<E> sucessor(Node<E> node) {
+		if (node == null) return null;
+		
+		Node<E> p = node.right;
+		if (p != null) {
+			while (p.left != null) {
+				p = p.left;
+			}
+			return p;
+		}
+		
+		while (node.parent != null && node == node.parent.right) {
+			node = node.parent;
+		}
+		
+		return node.parent;
+	}
+	
+	public static abstract class Visitor<E> {
+		boolean stop;
+		/**
+		 * @return 如果返回 true，表示停止遍历
+		 */
+		public abstract boolean visit(E element);
+	}
+	
+	protected static class Node<E> {
+		E element;
+		Node<E> left;
+		Node<E> right;
+		Node<E> parent;
+		public Node(E element, Node<E> parent) {
+			super();
+			this.element = element;
+			this.parent = parent;
+		}
+		
+		public boolean isLeaf() {
+			return left == null && right == null;
+		}
+		
+		public boolean hasTwoChildren() {
+			return left != null && right != null;
+		}
+		
+		public boolean isLeftChild() {
+			return parent != null && this == parent.left;
+		}
+		
+		public boolean isRightChild() {
+			return parent != null && this == parent.right;
+		}
+		
+		public Node<E> sibling() {
+			if (isLeftChild()) {
+				return parent.right;
+			}
+			
+			if (isRightChild()) {
+				return parent.left;
+			}
+			
+			return null;
+		}
+	}
+}
