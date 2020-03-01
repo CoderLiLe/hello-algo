3eeb4cb9d2aaec106f5a7846e4e82f58f64d782e
LiLe <1250124009@qq.com>
2020-03-01 18:40:40 +0800

    feat：剑指 60 Cpp
    面试题60: n个骰子的点数
     题目：把n个骰子扔在地上，所有骰子朝上一面的点数之和为s。输入n，打印出s
     的所有可能的值出现的概率。

--- 02-CPP/剑指Offer/剑指Offer/main.cpp
+++ 02-CPP/剑指Offer/剑指Offer/main.cpp
@@ -6,6 +6,17 @@
 //  Copyright © 2020 lile. All rights reserved.
 //
 
+/**
+ 
+ 知识迁移能力（53 ～ 59）
+    根据已掌握的知识、技术，能够快速学习、理解新的技术并应用到实际工作中。
+    由简单问题的思路引出复杂类似问题的解法。【举一反三】
+ 
+ 抽象建模能力（60 ～ 63）
+    1> 选择合适的数据结构来表述问题
+    2> 分析模型中的内在规律
+ 
+ */
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
