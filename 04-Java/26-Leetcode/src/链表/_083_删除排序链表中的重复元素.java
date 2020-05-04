0b3405ed3a4f7d71e67c483af359c72c68093da0
LiLe <1250124009@qq.com>
2020-05-04 23:11:07 +0800

    feat：Leetcode 刷题

--- 04-Java/26-Leetcode/src/链表/_083_删除排序链表中的重复元素.java
+++ 04-Java/26-Leetcode/src/链表/_083_删除排序链表中的重复元素.java
@@ -2,4 +2,9 @@ package 链表;
 
 public class _083_删除排序链表中的重复元素 {
 
+	public ListNode deleteDuplicates(ListNode head) {
+
+		return null;
+    }
+	
 }
--- 04-Java/26-Leetcode/src/链表/_206_反转链表.java
+++ 04-Java/26-Leetcode/src/链表/_206_反转链表.java
@@ -1,10 +1,27 @@
 package 链表;
 
 public class _206_反转链表 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	public ListNode reverseList1(ListNode head) {	
+		if (head == null || head.next == null) return head;
+		
+		ListNode newHead = reverseList1(head.next);
+		head.next.next = head;
+		head.next = null;
+		return newHead;
 	}
+	
+	public ListNode reverseList2(ListNode head) {
+		ListNode prev = null;
+		ListNode curr = head;
+		while (curr != null) {
+			ListNode nextTemp = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = nextTemp;
+		}
+		
+		return prev;
+    }
 
 }
