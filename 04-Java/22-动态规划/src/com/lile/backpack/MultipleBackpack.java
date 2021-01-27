d9459675b4edc80e232ad98edaf8d2ec17999152
李乐 <lile@aihujing.com>
2021-01-27 16:37:29 +0800

    feat：多重背包的二进制优化

--- 04-Java/22-动态规划/src/com/lile/backpack/MultipleBackpack.java
+++ 04-Java/22-动态规划/src/com/lile/backpack/MultipleBackpack.java
@@ -17,23 +17,60 @@ package com.lile.backpack;
  */
 
 public class MultipleBackpack {
+
+    // 物品种数
+    private int n = 4;
+    // 背包容积
+    private int v = 5;
+    // 体积
+    private int[] c = {1, 2, 3, 4};
+    // 价值
+    private int[] w = {2, 4, 4, 5};
+    // 可用数量
+    private int[] m = {3, 1, 3, 2};
+    private int[] dp = new int[105];
+
     public static void main(String[] args) {
-        // 物品种数
-        int n = 4;
-        // 背包容积
-        int v = 5;
-        // 体积
-        int[] c = {1, 2, 3, 4};
-        // 价值
-        int[] w = {2, 4, 4, 5};
-        // 可用数量
-        int[] m = {3, 1, 3, 2};
-        int[] dp = new int[105];
+        MultipleBackpack mb = new MultipleBackpack();
+        mb.multipleBackpack1();
+        mb.multipleBackpack2();
+    }
+
+    private void multipleBackpack1() {
         for (int i = 0; i < n; i++) { // 从小到大遍历物品
             for (int j = v; j >= c[i]; j--) { // 从大到小遍历背包
                 for (int k = 1; k <= m[i] && j >= k * c[i]; k++) { // 遍历第 i 种物品可选的数量
                     dp[j] = Math.max(dp[j], dp[j - k * c[i]] + k * w[i]);
                 }
+            }
+        }
+
+        System.out.println(dp[v]);
+    }
+
+    private void multipleBackpack2() {
+        int[] bulks = new int[4];
+        int[] values = new int[4];
+        int k = 0;
+
+        for (int i = 0; i < n; i++) { // 从小到大遍历物品
+            // 二进制优化
+            for (int j = 1; j < m[i]; j *= 2) {
+                bulks[k] = c[i] * j;
+                values[k++] = w[i] * j;
+                m[i] -= j;
+            }
+
+            if (m[i] > 0) {
+                bulks[k] = c[i] * m[i];
+                values[k++] = w[i] * m[i];
+            }
+        }
+
+        // 01背包
+        for (int i = 0; i < k; i++) {
+            for (int j = bulks[i]; j >= values[i]; j--) {
+                dp[j] = Math.max(dp[j], dp[j - values[i]] + bulks[i]);
             }
         }
 
