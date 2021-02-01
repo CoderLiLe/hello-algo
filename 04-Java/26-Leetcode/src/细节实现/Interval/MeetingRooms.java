4d501f1d8f41786136793a8113e25378b91dbd08
李乐 <lile@aihujing.com>
2021-02-01 16:54:10 +0800

    feat：252. Meeting Rooms

---
+++ 04-Java/26-Leetcode/src/细节实现/Interval/MeetingRooms.java
@@ -0,0 +1,53 @@
+package 细节实现.Interval;
+
+import tools.Asserts;
+
+import java.util.Arrays;
+
+/**
+ * 252. Meeting Rooms
+ *
+ * Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+ *
+ * Example 1:
+ *
+ * Input: intervals = [[0,30],[5,10],[15,20]]
+ * Output: false
+ * Example 2:
+ *
+ * Input: intervals = [[7,10],[2,4]]
+ * Output: true
+ *  
+ *
+ * Constraints:
+ *
+ * 0 <= intervals.length <= 104
+ * intervals[i].length == 2
+ * 0 <= starti < endi <= 106
+ */
+
+/**
+ * 因为一个人在同一时刻只能参加一个会议，因此题目实质是判断是否存在重叠区间，这个简单，将区间按照会议开始时间进行排序，然后遍历一遍判断即可。
+ */
+public class MeetingRooms {
+
+    public static void main(String[] args) {
+        int[][] intervals1 = {{0, 30}, {5, 10}, {15, 20}};
+        Asserts.test(canAttendMeetings(intervals1) == false);
+
+        int[][] intervals2 = {{7, 10}, {2, 4}};
+        Asserts.test(canAttendMeetings(intervals2) == true);
+    }
+
+    public static boolean canAttendMeetings(int[][] intervals) {
+        // 将区间按照会议开始时间生序排序
+        Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
+        // 遍历会议，如果下一个会议在前一个会议结束之前就开始了，返回 false
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < intervals[i - 1][1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
