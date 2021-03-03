1153c9b5238666c7bab8d82ef1e2e10fae70e1aa
李乐 <lile@aihujing.com>
2021-03-03 11:45:55 +0800

    fix：选择排序边界错误处理

--- 04-Java/15-排序/src/com/lile/sort/cmp/BubbleSort1.java
+++ 04-Java/15-排序/src/com/lile/sort/cmp/BubbleSort1.java
@@ -6,7 +6,7 @@ public class BubbleSort1<E extends Comparable<E>> extends Sort<E> {
 
 	@Override
 	protected void sort() {
-		for (int end = array.length-1; end > 1; end--) {
+		for (int end = array.length-1; end > 0; end--) {
 			for (int begin = 1; begin <= end; begin++) {
 				if (cmp(begin, begin - 1) < 0) {
 					swap(begin, begin - 1);
--- 04-Java/15-排序/src/com/lile/sort/cmp/BubbleSort3.java
+++ 04-Java/15-排序/src/com/lile/sort/cmp/BubbleSort3.java
@@ -6,7 +6,7 @@ public class BubbleSort3<E extends Comparable<E>> extends Sort<E> {
 
 	@Override
 	protected void sort() {
-		for (int end = array.length-1; end > 1; end--) {
+		for (int end = array.length-1; end > 0; end--) {
 			int sortedIndex = 1;
 			for (int begin = 1; begin <= end; begin++) {
 				if (cmp(begin, begin - 1) < 0) {
