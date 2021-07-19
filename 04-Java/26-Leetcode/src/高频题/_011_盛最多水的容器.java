02cf867a8ff4da9c79446693b594b85d1c78bb65
李乐 <lile@aihujing.com>
2021-07-19 17:41:27 +0800

    feat：11-盛最多水的容器，补充一个方法

--- 04-Java/26-Leetcode/src/高频题/_011_盛最多水的容器.java
+++ 04-Java/26-Leetcode/src/高频题/_011_盛最多水的容器.java
@@ -50,4 +50,18 @@ public class _011_盛最多水的容器 {
 
         return water;
     }
+
+    public int maxArea3(int[] height) {
+        if (height == null || height.length == 0) return 0;
+
+        int l = 0, r = height.length - 1, water = 0;
+        while (l < r) {
+            int minH = (height[l] <= height[r]) ? height[l] : height[r];
+            water = Math.max(water, (r - l) * minH);
+            while (l < r && height[l] <= minH) l++;
+            while (l < r && height[r] <= minH) r--;
+        }
+
+        return water;
+    }
 }
