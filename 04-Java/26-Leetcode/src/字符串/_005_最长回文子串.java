41b6099328a3939e1cb3f15be6761052cbfa61fd
lile <1250124009@qq.com>
2024-12-24 13:48:42 +0800

    feat：最长回文子串-暴力法

--- 04-Java/26-Leetcode/src/字符串/_005_最长回文子串.java
+++ 04-Java/26-Leetcode/src/字符串/_005_最长回文子串.java
@@ -1,5 +1,37 @@
 package 字符串;
 
-public class _005_最长回文子串 {
+import tools.Asserts;
 
+import java.util.Scanner;
+
+public class _005_最长回文子串 {
+    public static void main(String[] args) {
+        Asserts.test(longestPalindrome("cdabbacc") == 4);
+    }
+
+    private static boolean isPalindromic(String s) {
+        int len = s.length();
+        for (int i = 0; i < len / 2; i++) {
+            if (s.charAt(i) != s.charAt(len - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 暴力解法
+    private static int longestPalindrome(String s) {
+        String ans = "";
+        int max = 0;
+        int len = s.length();
+        for (int i = 0; i < len; i++)
+            for (int j = i + 1; j <= len; j++) {
+                String test = s.substring(i, j);
+                if (isPalindromic(test) && test.length() > max) {
+                    ans = s.substring(i, j);
+                    max = Math.max(max, ans.length());
+                }
+            }
+        return max;
+    }
 }
