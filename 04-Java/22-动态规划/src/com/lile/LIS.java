819591ddd8db28158838c6cdb681e64f5b745143
lile <1250124009@qq.com>
2025-01-02 19:34:46 +0800

    feat：LIS

--- 04-Java/22-动态规划/src/com/lile/LIS.java
+++ 04-Java/22-动态规划/src/com/lile/LIS.java
@@ -1,9 +1,20 @@
 package com.lile;
 
+import com.lile.tools.Asserts;
+import com.lile.tools.Times;
+
 public class LIS {
 
 	public static void main(String[] args) {
-		System.out.println(lengthOfLIS(new int[] {10, 2, 2, 5, 1, 7, 101, 18}));
+		Times.test("二分搜索", () -> {
+			Asserts.test(lengthOfLIS(new int[] {10, 2, 2, 5, 1, 7, 101, 18}) == 4);
+		});
+		Times.test("二分搜索2（优化）", () -> {
+			Asserts.test(lengthOfLIS2(new int[] {10, 2, 2, 5, 1, 7, 101, 18}) == 4);
+		});
+		Times.test("动态规划", () -> {
+			Asserts.test(lengthOfLIS1(new int[] {10, 2, 2, 5, 1, 7, 101, 18}) == 4);
+		});
 	}
 	
 	/**
@@ -67,12 +78,12 @@ public class LIS {
 	static int lengthOfLIS1(int[] nums) {
 		if (nums == null || nums.length == 0) return 0;
 		int[] dp = new int[nums.length];
-		int max = dp[0] =1;
+		int max = dp[0] = 1;
 		for (int i = 1; i < nums.length; i++) {
 			dp[i] = 1;
 			for (int j = 0; j < i; j++) {
 				if (nums[i] <= nums[j]) continue;
-				dp[i] = Math.max(max, dp[j] + 1);
+				dp[i] = Math.max(dp[i], dp[j] + 1);
 			}
 			max = Math.max(max, dp[i]);
 		}
