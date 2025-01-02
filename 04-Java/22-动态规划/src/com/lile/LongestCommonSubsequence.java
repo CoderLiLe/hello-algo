ed69493107d5249e1fdf487b3f43323435fd9082
lile <1250124009@qq.com>
2025-01-02 20:44:28 +0800

    feat：修改名字，最长公共子序列 Longest Common Subsequence

--- 04-Java/22-动态规划/src/com/lile/LongestCommonSubsequence.java
+++ 04-Java/22-动态规划/src/com/lile/LongestCommonSubsequence.java
@@ -7,8 +7,15 @@ import com.lile.tools.Times;
  * 最长公共子序列 Longest Common Subsequence
  * @author LiLe
  * Leetcode_1143
+ *
+ * 给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+ *
+ * 一个字符串的 子序列 是指这样一个新的字符串：它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+ *
+ * 例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+ * 两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
  */
-public class LCS {
+public class LongestCommonSubsequence {
 
 	public static void main(String[] args) {
 		int[] nums1 = new int[] {1, 4, 5, 9, 10};
