7ac1daf67cbfd49528d398ae42ea2964952b5ee4
lile <1250124009@qq.com>
2025-01-02 20:11:43 +0800

    feat：1143. 最长公共子序列

--- 04-Java/22-动态规划/src/com/lile/LCS.java
+++ 04-Java/22-动态规划/src/com/lile/LCS.java
@@ -1,4 +1,8 @@
 package com.lile;
+
+import com.lile.tools.Asserts;
+import com.lile.tools.Times;
+
 /**
  * 最长公共子序列 Longest Common Subsequence
  * @author LiLe
@@ -7,8 +11,33 @@ package com.lile;
 public class LCS {
 
 	public static void main(String[] args) {
-		System.out.println(lcs(new int[] {1, 4, 5, 9, 10}, new int[] {1, 4, 9, 10}));
-	} 
+		int[] nums1 = new int[] {1, 4, 5, 9, 10};
+		int[] nums2 = new int[] {1, 4, 9, 10};
+
+		Times.test("递归实现", () -> {
+			Asserts.test(lcs1(nums1, nums2) == 4);
+		});
+		Times.test("非递归实现", () -> {
+			Asserts.test(lcs2(nums1, nums2) == 4);
+		});
+		Times.test("非递归实现 + 滚动数组", () -> {
+			Asserts.test(lcs3(nums1, nums2) == 4);
+		});
+		Times.test("非递归实现 + 滚动数组", () -> {
+			Asserts.test(lcs4(nums1, nums2) == 4);
+		});
+		Times.test("非递归实现 + 滚动数组 优化", () -> {
+			Asserts.test(lcs(nums1, nums2) == 4);
+		});
+
+		String text1 = "abcde", text2 = "ace";
+		Times.test("字符串-二维数组", () -> {
+			Asserts.test(longestCommonSubsequence1(text1, text2) == 3);
+		});
+		Times.test("字符串-一维数组", () -> {
+			Asserts.test(longestCommonSubsequence2(text1, text2) == 3);
+		});
+	}
 	
 	// 非递归实现 + 滚动数组 优化
 	static int lcs(int[] nums1, int[] nums2) {
@@ -112,22 +141,40 @@ public class LCS {
 		return Math.max(lcs1(nums1, i, nums2, j - 1), lcs1(nums1, i - 1, nums2, j));
 	}
 	
-	// 非递归实现 + 滚动数组 
-	static int longestCommonSubsequence(String text1, String text2) {
+
+	// 字符串-二维数组
+	private static int longestCommonSubsequence1(String text1, String text2) {
+		int[][] dp = new int[text1.length() + 1][text2.length() + 1]; // 先对dp数组做初始化操作
+		for (int i = 1 ; i <= text1.length() ; i++) {
+			char char1 = text1.charAt(i - 1);
+			for (int j = 1; j <= text2.length(); j++) {
+				char char2 = text2.charAt(j - 1);
+				if (char1 == char2) { // 开始列出状态转移方程
+					dp[i][j] = dp[i - 1][j - 1] + 1;
+				} else {
+					dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+				}
+			}
+		}
+		return dp[text1.length()][text2.length()];
+	}
+
+	// 字符串-一维数组
+	private static int longestCommonSubsequence2(String text1, String text2) {
 		if (text1 == null || text2 == null) return 0;
 		char[] chars1 = text1.toCharArray();
 		if (chars1.length == 0) return 0;
 		char[] chars2 = text2.toCharArray();
 		if (chars2.length == 0) return 0;
-		
+
 		char[] rowChars = chars1, colChars = chars2;
 		if (chars1.length < chars2.length) {
 			rowChars = chars2;
 			colChars = chars1;
 		}
-	
+
 		int[] dp = new int[colChars.length + 1];
-		
+
 		for (int i = 1; i <= rowChars.length; i++) {
 			int cur = 0;
 			for (int j = 1; j <= colChars.length; j++) {
