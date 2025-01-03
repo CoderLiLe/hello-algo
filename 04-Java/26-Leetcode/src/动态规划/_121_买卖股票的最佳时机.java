e09cca9e6bcaaa80dec827c8884081ae51ca4cb8
lile <1250124009@qq.com>
2025-01-03 18:33:26 +0800

    feat：121_买卖股票的最佳时机

--- 04-Java/26-Leetcode/src/动态规划/_121_买卖股票的最佳时机.java
+++ 04-Java/26-Leetcode/src/动态规划/_121_买卖股票的最佳时机.java
@@ -1,11 +1,141 @@
 package 动态规划;
 
+import tools.Asserts;
+import tools.Times;
+
 /**
  * https://leetcode-cn.com/problems/best-time-to-buy-and-sell-stock/
  */
 public class _121_买卖股票的最佳时机 {
-    public int maxProfit(int[] prices) {
-        if (prices == null || prices.length == 0) return 0;
+
+    public static void main(String[] args) {
+        int[] prices = new int[] {7, 1, 5, 3, 6, 4};
+        Times.test("暴力法【超时】", () -> {
+            Asserts.test(maxProfit1(prices) == 5);
+        });
+        Times.test("贪心法", () -> {
+            Asserts.test(maxProfit2(prices) == 5);
+        });
+        Times.test("动态规划：二维数组", () -> {
+            Asserts.test(maxProfit3(prices) == 5);
+        });
+        Times.test("动态规划：使用二维滚动數組", () -> {
+            Asserts.test(maxProfit4(prices) == 5);
+        });
+        Times.test("动态规划：使用一维滚动數組", () -> {
+            Asserts.test(maxProfit5(prices) == 5);
+        });
+        Times.test("抽象动态规划", () -> {
+            Asserts.test(maxProfit6(prices) == 5);
+        });
+    }
+
+    /**
+     *  暴力法【超时】
+     *  时间复杂度：O(n^2)
+     *  空间复杂度：O(1)
+     */
+    private static int maxProfit1(int[] prices) {
+        int result = 0;
+        for (int i = 0; i < prices.length; i++) {
+            for (int j = i + 1; j < prices.length; j++){
+                result = Math.max(result, prices[j] - prices[i]);
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 贪心法
+     * 因为股票就买卖一次，那么贪心的想法很自然就是取最左最小值，取最右最大值，那么得到的差值就是最大利润。
+     *
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     */
+    private static int maxProfit2(int[] prices) {
+        int low = Integer.MAX_VALUE;
+        int result = 0;
+        for (int i = 0; i < prices.length; i++) {
+            // 取最左最小价格
+            low = Math.min(low, prices[i]);
+            // 直接取最大区间利润
+            result = Math.max(result, prices[i] - low);
+        }
+        return result;
+    }
+
+    /**
+     * 动态规划：二维数组
+     */
+    private static int maxProfit3(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
+        int length = prices.length;
+
+        // dp[i][0]代表第i天持有股票的最大收益
+        // dp[i][1]代表第i天不持有股票的最大收益
+        int[][] dp = new int[length][2];
+        dp[0][0] = -prices[0];
+        dp[0][1] = 0;
+        for (int i = 1; i < length; i++) {
+            dp[i][0] = Math.max(dp[i - 1][0], -prices[i]);
+            dp[i][1] = Math.max(dp[i - 1][0] + prices[i], dp[i - 1][1]);
+        }
+        return dp[length - 1][1];
+    }
+
+    /**
+     * 动态规划：使用二维滚动數組
+     */
+    private static int maxProfit4(int[] prices) {
+        int len = prices.length;
+        int dp[][] = new int[2][2];
+
+        dp[0][0] = - prices[0];
+        dp[0][1] = 0;
+
+        for (int i = 1; i < len; i++){
+            dp[i % 2][0] = Math.max(dp[(i - 1) % 2][0], - prices[i]);
+            dp[i % 2][1] = Math.max(dp[(i - 1) % 2][1], prices[i] + dp[(i - 1) % 2][0]);
+        }
+        return dp[(len - 1) % 2][1];
+    }
+
+    /**
+     * 动态规划：使用一维滚动數組
+     */
+    private static int maxProfit5(int[] prices) {
+        int[] dp = new int[2];
+        // 记录一次交易，一次交易有买入卖出两种状态
+        // 0代表持有，1代表卖出
+        dp[0] = -prices[0];
+        dp[1] = 0;
+        // 可以参考斐波那契问题的优化方式
+        // 我们从 i=1 开始遍历数组，一共有 prices.length 天，
+        // 所以是 i<=prices.length
+        for (int i = 1; i <= prices.length; i++) {
+            // 前一天持有；或当天买入
+            dp[0] = Math.max(dp[0], -prices[i - 1]);
+            // 如果 dp[0] 被更新，那么 dp[1] 肯定会被更新为正数的 dp[1]
+            // 而不是 dp[0]+prices[i-1]==0 的0，
+            // 所以这里使用会改变的dp[0]也是可以的
+            // 当然 dp[1] 初始值为 0 ，被更新成 0 也没影响
+            // 前一天卖出；或当天卖出, 当天要卖出，得前一天持有才行
+            dp[1] = Math.max(dp[1], dp[0] + prices[i - 1]);
+        }
+        return dp[1];
+    }
+
+    /**
+     * 抽象动态规划
+     */
+    private static int maxProfit6(int[] prices) {
+        if (prices == null || prices.length == 0) {
+            return 0;
+        }
+
         // 前面扫描过的最小价格
         int minPrice = prices[0];
         // 前面扫描过的最大利润
