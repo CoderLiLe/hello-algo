630c0ead567e1bc6301b13c97fe5943b14b8f3f4
lile <1250124009@qq.com>
2025-01-22 01:34:44 +0800

    feat：74搜索二维矩阵

--- 04-Java/26-Leetcode/src/排序_数组/_074搜索二维矩阵.java
+++ 04-Java/26-Leetcode/src/排序_数组/_074搜索二维矩阵.java
@@ -6,6 +6,59 @@ package 排序_数组;
  */
 public class _074搜索二维矩阵 {
     /**
+     * 直接遍历整个矩阵 matrix，判断 target 是否出现
+     * 时间复杂度：O(mn)。
+     * 空间复杂度：O(1)
+     *
+     * @param matrix
+     * @param target
+     * @return
+     */
+    public boolean searchMatrix(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            for (int element : row) {
+                if (element == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 由于矩阵 matrix 中每一行的元素都是升序排列的，因此我们可以对每一行都使用一次二分查找，
+     * 判断 target 是否在该行中，从而判断 target 是否出现
+     *
+     * 时间复杂度：O(mlogn)。对一行使用二分查找的时间复杂度为 O(logn)，最多需要进行 m 次二分查找。
+     * 空间复杂度：O(1)。
+     */
+    public boolean searchMatrix2(int[][] matrix, int target) {
+        for (int[] row : matrix) {
+            int index = search(row, target);
+            if (index >= 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            int num = nums[mid];
+            if (num == target) {
+                return mid;
+            } else if (num > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+
+    /**
      * 两次二分查找
      * 时间复杂度：O(logm+logn)=O(logmn)，其中 m 和 n 分别是矩阵的行数和列数。
      * 空间复杂度：O(1)。
@@ -14,7 +67,7 @@ public class _074搜索二维矩阵 {
      * @param target
      * @return
      */
-    public boolean searchMatrix(int[][] matrix, int target) {
+    public boolean searchMatrix3(int[][] matrix, int target) {
         int minRow = -1, maxRow = matrix.length - 1;
         int minCol = 0, maxCol = matrix[0].length - 1;
 
@@ -55,7 +108,7 @@ public class _074搜索二维矩阵 {
      * @param target
      * @return
      */
-    public boolean searchMatrix2(int[][] matrix, int target) {
+    public boolean searchMatrix4(int[][] matrix, int target) {
         int rowNum = matrix.length;
         int colNum = matrix[0].length;
         int left = 0, right = rowNum * colNum - 1;
@@ -76,6 +129,7 @@ public class _074搜索二维矩阵 {
     }
 
     /**
+     * Z 字形查找
      * 在一个二维矩阵中搜索一个特定的目标值。
      * 该矩阵的特点是每行的元素从左到右按升序排列，每列的元素从上到下按升序排列。
      * 该方法从矩阵的右上角开始搜索，如果当前元素大于目标值，则当前元素所在列的元素全部大于目标值，排除该列；
@@ -88,7 +142,7 @@ public class _074搜索二维矩阵 {
      * @param target 需要搜索的目标值。
      * @return 如果矩阵中存在目标值，则返回 true；否则返回 false。
      */
-    public boolean searchMatrix3(int[][] matrix, int target) {
+    public boolean searchMatrix5(int[][] matrix, int target) {
         // 初始化行位置为0，即从第一行开始搜索
         int row = 0;
         // 初始化列位置为最后一列，即从右上角开始搜索
@@ -113,6 +167,7 @@ public class _074搜索二维矩阵 {
     }
 
     /**
+     * Z 字形查找
      * 在一个二维矩阵中搜索目标值。矩阵的每行和每列都是升序排列的。
      * 该方法从矩阵的左下角开始搜索，利用矩阵的有序特性，逐步缩小搜索范围，直到找到目标值或搜索范围为空。
      *
@@ -123,7 +178,7 @@ public class _074搜索二维矩阵 {
      * @param target 目标值。
      * @return 如果矩阵中包含目标值，则返回true；否则返回false。
      */
-    public boolean searchMatrix4(int[][] matrix, int target) {
+    public boolean searchMatrix6(int[][] matrix, int target) {
         if (matrix == null || matrix.length < 1 || matrix[0].length < 1) {
             return false;
         }
