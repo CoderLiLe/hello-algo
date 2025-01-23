b4803c24811de5e261be89419eb721b0f28f2c5f
lile <1250124009@qq.com>
2025-01-23 22:44:31 +0800

    feat：4. 寻找两个正序数组的中位数

--- 04-Java/26-Leetcode/src/排序_数组/_004_寻找两个正序数组中的中位数.java
+++ 04-Java/26-Leetcode/src/排序_数组/_004_寻找两个正序数组中的中位数.java
@@ -1,9 +1,12 @@
 package 排序_数组;
 
+import tools.Asserts;
+import tools.Times;
+
 /**
  * 4. 寻找两个正序数组的中位数
  * 困难
- * 给定两个大小为 m 和 n 的正序（从小到大）数组 nums1 和 nums2。请你找出并返回这两个正序数组的中位数。
+ * 给定两个大小为 m 和 n 的正序（从小到大）数组nums1 和nums2。请你找出并返回这两个正序数组的中位数。
  *
  * 进阶：你能设计一个时间复杂度为 O(log (m+n)) 的算法解决此问题吗？
  *
@@ -28,10 +31,8 @@ package 排序_数组;
  * 示例 5：
  * 输入：nums1 = [2], nums2 = []
  * 输出：2.00000
- *  
  *
  * 提示：
- *
  * nums1.length == m
  * nums2.length == n
  * 0 <= m <= 1000
@@ -41,9 +42,94 @@ package 排序_数组;
  */
 public class _004_寻找两个正序数组中的中位数 {
 
-    private double findMedianSortedArrays1(int[] nums1, int[] nums2) {
+    /**
+     * 先将两个数组合并，两个有序数组的合并也是归并排序中的一部分。然后根据奇数，还是偶数，返回中位数
+     * 时间复杂度：遍历全部数组 (m+n)
+     * 空间复杂度：开辟了一个数组，保存合并后的两个数组 O(m+n)
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] nums;
+        int m = nums1.length;
+        int n = nums2.length;
+        nums = new int[m + n];
+        if (m == 0) {
+            if (n % 2 == 0) {
+                return (nums2[n / 2 - 1] + nums2[n / 2]) / 2.0;
+            } else {
 
-        return 2.0;
+                return nums2[n / 2];
+            }
+        }
+        if (n == 0) {
+            if (m % 2 == 0) {
+                return (nums1[m / 2 - 1] + nums1[m / 2]) / 2.0;
+            } else {
+                return nums1[m / 2];
+            }
+        }
+
+        int count = 0;
+        int i = 0, j = 0;
+        while (count != (m + n)) {
+            if (i == m) {
+                while (j != n) {
+                    nums[count++] = nums2[j++];
+                }
+                break;
+            }
+            if (j == n) {
+                while (i != m) {
+                    nums[count++] = nums1[i++];
+                }
+                break;
+            }
+
+            if (nums1[i] < nums2[j]) {
+                nums[count++] = nums1[i++];
+            } else {
+                nums[count++] = nums2[j++];
+            }
+        }
+
+        if (count % 2 == 0) {
+            return (nums[count / 2 - 1] + nums[count / 2]) / 2.0;
+        } else {
+            return nums[count / 2];
+        }
+    }
+
+    /**
+     * 不需要将两个数组真的合并，我们只需要找到中位数在哪里就可以了
+     * 时间复杂度: O(m+n)
+     * 空间复杂度：O(1)
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        int len = m + n;
+        int left = -1, right = -1;
+        int aStart = 0, bStart = 0;
+        for (int i = 0; i <= len / 2; i++) {
+            left = right;
+            if (aStart < m && (bStart >= n || nums1[aStart] < nums2[bStart])) {
+                right = nums1[aStart++];
+            } else {
+                right = nums2[bStart++];
+            }
+        }
+        if ((len & 1) == 0) {
+            return (left + right) / 2.0;
+        } else {
+            return right;
+        }
     }
 
     /**
@@ -52,7 +138,7 @@ public class _004_寻找两个正序数组中的中位数 {
      * 时间复杂度：每进行一次循环，我们就减少 k/2 个元素，所以时间复杂度是 O(log(k)，而 k=(m+n)/2，所以最终的复杂也就是 O(log(m+n）。
      * 空间复杂度：虽然我们用到了递归，但是可以看到这个递归属于尾递归，所以编译器不需要不停地堆栈，所以空间复杂度为 O(1)
      */
-    private double findMedianSortedArrays2(int[] nums1, int[] nums2) {
+    public double findMedianSortedArrays3(int[] nums1, int[] nums2) {
         int n = nums1.length;
         int m = nums2.length;
         int left = (n + m + 1) / 2;
@@ -64,10 +150,16 @@ public class _004_寻找两个正序数组中的中位数 {
     private int getKth(int[] nums1, int start1, int end1, int[] nums2, int start2, int end2, int k) {
         int len1 = end1 - start1 + 1;
         int len2 = end2 - start2 + 1;
-        if (len1 > len2) return getKth(nums2, start2, end2, nums1, start1, end1, k);
-        if (len1 == 0) return nums2[start2 + k - 1];
+        if (len1 > len2) {
+            return getKth(nums2, start2, end2, nums1, start1, end1, k);
+        };
+        if (len1 == 0) {
+            return nums2[start2 + k - 1];
+        }
 
-        if (k == 1) return Math.min(nums1[start1], nums2[start2]);
+        if (k == 1) {
+            return Math.min(nums1[start1], nums2[start2]);
+        }
 
         int i = start1 + Math.min(len1, k / 2) - 1;
         int j = start2 + Math.min(len2, k / 2) - 1;
@@ -79,11 +171,75 @@ public class _004_寻找两个正序数组中的中位数 {
         }
     }
 
-    public static void main(String[] args) {
-        int[] nums1 = {1, 3};
-        int[] nums2 = {2};
+    /**
+     * 时间复杂度：我们对较短的数组进行了二分查找，所以时间复杂度是 O(log(min(m，n))。
+     * 空间复杂度：只有一些固定的变量，和数组长度无关，所以空间复杂度是 O(1)。
+     *
+     *
+     * @param nums1
+     * @param nums2
+     * @return
+     */
+    public double findMedianSortedArrays4(int[] nums1, int[] nums2) {
+        int m = nums1.length;
+        int n = nums2.length;
+        if (m > n) {
+            // 保证 m <= n
+            return findMedianSortedArrays(nums2, nums1);
+        }
+        int iMin = 0, iMax = m;
+        while (iMin <= iMax) {
+            int i = (iMin + iMax) / 2;
+            int j = (m + n + 1) / 2 - i;
+            if (j != 0 && i != m && nums2[j - 1] > nums1[i]) { // i 需要增大
+                iMin = i + 1;
+            } else if (i != 0 && j != n && nums1[i - 1] > nums2[j]) { // i 需要减小
+                iMax = i - 1;
+            } else { // 达到要求，并且将边界条件列出来单独考虑
+                int maxLeft = 0;
+                if (i == 0) {
+                    maxLeft = nums2[j - 1];
+                } else if (j == 0) {
+                    maxLeft = nums1[i - 1];
+                } else {
+                    maxLeft = Math.max(nums1[i - 1], nums2[j - 1]);
+                }
+                if ((m + n) % 2 == 1) {
+                    return maxLeft;
+                } // 奇数的话不需要考虑右半部分
 
-        _004_寻找两个正序数组中的中位数 medianNum = new _004_寻找两个正序数组中的中位数();
-        System.out.println(medianNum.findMedianSortedArrays1(nums1, nums2));
+                int minRight = 0;
+                if (i == m) {
+                    minRight = nums2[j];
+                } else if (j == n) {
+                    minRight = nums1[i];
+                } else {
+                    minRight = Math.min(nums2[j], nums1[i]);
+                }
+
+                return (maxLeft + minRight) / 2.0; //如果是偶数的话返回结果
+            }
+        }
+        return 0.0;
+    }
+
+    public static void main(String[] args) {
+        _004_寻找两个正序数组中的中位数 obj = new _004_寻找两个正序数组中的中位数();
+
+        int[] nums1 = {1, 3, 5, 6, 8, 9, 10, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59, 61, 63, 65, 67, 69, 71,73, 75, 77, 79, 81};
+        int[] nums2 = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54,56, 58, 60, 62, 64, 66, 68, 70, 72, 74, 76, 78, 80, 82, 84, 86, 88, 90, 92, 94, 96, 98, 100};
+        double res = 47.5;
+        Times.test("方法一", () -> {
+            Asserts.test(obj.findMedianSortedArrays(nums1, nums2) == res);
+        });
+        Times.test("方法二", () -> {
+            Asserts.test(obj.findMedianSortedArrays2(nums1, nums2) == res);
+        });
+        Times.test("方法三", () -> {
+            Asserts.test(obj.findMedianSortedArrays3(nums1, nums2) == res);
+        });
+        Times.test("方法四", () -> {
+            Asserts.test(obj.findMedianSortedArrays4(nums1, nums2) == res);
+        });
     }
 }
