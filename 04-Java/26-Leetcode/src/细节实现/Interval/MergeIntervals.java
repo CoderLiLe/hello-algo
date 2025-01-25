4227dffec250f6bf5bc83590e5483f293f24f0f9
lile <1250124009@qq.com>
2025-01-25 12:08:14 +0800

    feat：去掉多余字符

--- 04-Java/26-Leetcode/src/细节实现/Interval/MergeIntervals.java
+++ 04-Java/26-Leetcode/src/细节实现/Interval/MergeIntervals.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 /**
  * 56. Merge Intervals
- * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
+ * Given an array of intervals where intervals[i] = [starti, endi], merge all overlapping intervals, and return an array of the non-overlapping intervals that cover all the intervals in the input.
  *
  * Example 1:
  *
