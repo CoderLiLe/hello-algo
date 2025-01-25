44741e000ba6579ff8e822ed170b4d943cb3b2a2
lile <1250124009@qq.com>
2025-01-25 13:30:43 +0800

    feat:27_移除元素

--- 04-Java/26-Leetcode/src/TwoPoints/_027_移除元素.java
+++ 04-Java/26-Leetcode/src/TwoPoints/_027_移除元素.java
@@ -13,4 +13,29 @@ public class _027_移除元素 {
 
         return slow;
     }
+
+    public int removeElement2(int[] nums, int val) {
+        int j = nums.length - 1;
+        for (int i = 0; i <= j; i++) {
+            if (nums[i] == val) {
+                swap(nums, i--, j--);
+            }
+        }
+        return j + 1;
+    }
+    void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+
+    public int removeElement3(int[] nums, int val) {
+        int idx = 0;
+        for (int x : nums) {
+            if (x != val) {
+                nums[idx++] = x;
+            }
+        }
+        return idx;
+    }
 }
