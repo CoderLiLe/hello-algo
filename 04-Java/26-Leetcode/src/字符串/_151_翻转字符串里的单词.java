411773148952832b2af7bfe52ad535a5063ece69
lile <1250124009@qq.com>
2025-01-27 20:02:11 +0800

    feat:151_翻转字符串里的单词

--- 04-Java/26-Leetcode/src/字符串/_151_翻转字符串里的单词.java
+++ 04-Java/26-Leetcode/src/字符串/_151_翻转字符串里的单词.java
@@ -1,7 +1,29 @@
 package 字符串;
 
 public class _151_翻转字符串里的单词 {
-	public String reverseWords(String s) {
+	/**
+	 * 分割 + 倒序
+	 * 时间复杂度：O(N)
+	 * 空间复杂度：O(N)
+	 * @param s
+	 * @return
+	 */
+	public String reverseWords1(String s) {
+		if (s == null || s.isEmpty()) {
+			return s;
+		}
+
+		String[] strings = s.split(" ");
+		StringBuilder sb = new StringBuilder();
+		for (int i = strings.length - 1; i >= 0; i--) {
+			if (!strings[i].isEmpty()) {
+				sb.append(strings[i]).append(" ");
+			}
+		}
+		return sb.toString().trim();
+	}
+
+	public String reverseWords2(String s) {
 		if (s == null) return "";
 		char[] chars = s.toCharArray();
 		
@@ -54,5 +76,31 @@ public class _151_翻转字符串里的单词 {
 			li++;
 			ri--;
 		}
+	}
+
+	/**
+	 * 双指针
+	 * 算法解析：
+	 * 倒序遍历字符串 s ，记录单词左右索引边界 i , j 。
+	 * 每确定一个单词的边界，则将其添加至单词列表 res 。
+	 * 最终，将单词列表拼接为字符串，并返回即可。
+	 *
+	 * 时间复杂度 O(N) ： 其中 N 为字符串 s 的长度，线性遍历字符串。
+	 * 空间复杂度 O(N) ： 新建的 list(Python) 或 StringBuilder(Java) 中的字符串总长度 ≤N ，占用 O(N) 大小的额外空间。
+	 *
+	 * @param s
+	 * @return
+	 */
+	public String reverseWords3(String s) {
+		s = s.trim();                                    // 删除首尾空格
+		int j = s.length() - 1, i = j;
+		StringBuilder res = new StringBuilder();
+		while (i >= 0) {
+			while (i >= 0 && s.charAt(i) != ' ') i--;     // 搜索首个空格
+			res.append(s.substring(i + 1, j + 1) + " "); // 添加单词
+			while (i >= 0 && s.charAt(i) == ' ') i--;     // 跳过单词间空格
+			j = i;                                       // j 指向下个单词的尾字符
+		}
+		return res.toString().trim();                    // 转化为字符串并返回
 	}
 }
