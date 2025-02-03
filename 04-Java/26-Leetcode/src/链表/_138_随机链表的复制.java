a35159db6e9cc8096ea1741d96daa32556c2e445
lile <1250124009@qq.com>
2025-02-03 10:52:28 +0800

    feat:138_随机链表的复制

--- 04-Java/26-Leetcode/src/链表/_138_随机链表的复制.java
+++ 04-Java/26-Leetcode/src/链表/_138_随机链表的复制.java
@@ -28,27 +28,27 @@ public class _138_随机链表的复制 {
      * 哈希表
      * 利用哈希表的查询特点，考虑构建 原链表节点 和 新链表对应节点 的键值对映射关系，
      * 再遍历构建新链表各节点的 next 和 random 引用指向即可
-     *
+     * <p>
      * 时间复杂度 O(N) ： 遍历链表两次，使用 O(N) 时间。
      * 空间复杂度 O(N) ： 节点引用变量使用 O(N) 大小的额外空间
      */
     public Node copyRandomList(Node head) {
         // 1. 若头节点 head 为空节点，直接返回 null 。
-        if(head == null) return null;
+        if (head == null) return null;
 
         // 2. 初始化： 哈希表 dic ， 节点 cur 指向头节点。
         Node cur = head;
         Map<Node, Node> map = new HashMap<>();
 
         // 3. 复制各节点，并建立 “原节点 -> 新节点” 的 Map 映射
-        while(cur != null) {
+        while (cur != null) {
             map.put(cur, new Node(cur.val));
             cur = cur.next;
         }
 
         cur = head;
         // 4. 构建新链表的 next 和 random 指向
-        while(cur != null) {
+        while (cur != null) {
             map.get(cur).next = map.get(cur.next);
             map.get(cur).random = map.get(cur.random);
             cur = cur.next;
@@ -62,16 +62,16 @@ public class _138_随机链表的复制 {
      * 拼接 + 拆分
      * 构建 原节点 1 -> 新节点 1 -> 原节点 2 -> 新节点 2 -> …… 的拼接链表，
      * 如此便可在访问原节点的 random 指向节点的同时找到新对应新节点的 random 指向节点
-     *
+     * <p>
      * 时间复杂度 O(N) ： 三轮遍历链表，使用 O(N) 时间。
      * 空间复杂度 O(1) ： 节点引用变量使用常数大小的额外空间
      */
     public Node copyRandomList2(Node head) {
-        if(head == null) return null;
+        if (head == null) return null;
         Node cur = head;
 
         // 1. 复制各节点，并构建拼接链表
-        while(cur != null) {
+        while (cur != null) {
             Node tmp = new Node(cur.val);
             tmp.next = cur.next;
             cur.next = tmp;
@@ -80,8 +80,8 @@ public class _138_随机链表的复制 {
 
         // 2. 构建各新节点的 random 指向
         cur = head;
-        while(cur != null) {
-            if(cur.random != null)
+        while (cur != null) {
+            if (cur.random != null)
                 cur.next.random = cur.random.next;
             cur = cur.next.next;
         }
@@ -89,7 +89,7 @@ public class _138_随机链表的复制 {
         // 3. 拆分两链表
         cur = head.next;
         Node pre = head, res = head.next;
-        while(cur.next != null) {
+        while (cur.next != null) {
             pre.next = pre.next.next;
             cur.next = cur.next.next;
             pre = pre.next;
@@ -125,6 +125,7 @@ public class _138_随机链表的复制 {
     static class Node {
         int val;
         Node next, random;
+
         public Node(int val) {
             this.val = val;
             this.next = null;
