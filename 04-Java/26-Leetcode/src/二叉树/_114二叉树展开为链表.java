3c4b7a2d62671eb733543e77e0473cf9088b231c
lile <1250124009@qq.com>
2025-02-04 12:30:59 +0800

    feat:144二叉树的前序遍历

--- 04-Java/26-Leetcode/src/二叉树/_114二叉树展开为链表.java
+++ 04-Java/26-Leetcode/src/二叉树/_114二叉树展开为链表.java
@@ -2,6 +2,11 @@ package 二叉树;
 
 import common.TreeNode;
 
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
 public class _114二叉树展开为链表 {
     /**
      * 定义：将以 root 为根的树拉平为链表
@@ -29,6 +34,108 @@ public class _114二叉树展开为链表 {
             p = p.right;
         }
         p.right = right;
+    }
+
+    /**
+     * 将二叉树展开为单链表之后，单链表中的节点顺序即为二叉树的前序遍历访问各节点的顺序
+     */
+
+    /**
+     * 递归实现前序遍历
+     */
+    public void flatten2(TreeNode root) {
+        List<TreeNode> list = new ArrayList<>();
+        preorderTraversal(root, list);
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.left = null;
+            prev.right = curr;
+        }
+    }
+
+    public void preorderTraversal(TreeNode root, List<TreeNode> list) {
+        if (root != null) {
+            list.add(root);
+            preorderTraversal(root.left, list);
+            preorderTraversal(root.right, list);
+        }
+    }
+
+    /**
+     * 迭代实现前序遍历
+     */
+    public void flatten3(TreeNode root) {
+        List<TreeNode> list = new ArrayList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+        while (node != null || !stack.isEmpty()) {
+            while (node != null) {
+                list.add(node);
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            node = node.right;
+        }
+        int size = list.size();
+        for (int i = 1; i < size; i++) {
+            TreeNode prev = list.get(i - 1), curr = list.get(i);
+            prev.left = null;
+            prev.right = curr;
+        }
+    }
+
+    /**
+     * 前序遍历和展开同步进行
+     */
+    public void flatten4(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        Deque<TreeNode> stack = new LinkedList<>();
+        stack.push(root);
+        TreeNode prev = null;
+        while (!stack.isEmpty()) {
+            TreeNode curr = stack.pop();
+            if (prev != null) {
+                prev.left = null;
+                prev.right = curr;
+            }
+            TreeNode left = curr.left, right = curr.right;
+            if (right != null) {
+                stack.push(right);
+            }
+            if (left != null) {
+                stack.push(left);
+            }
+            prev = curr;
+        }
+    }
+
+    /**
+     * 对于当前节点，如果其左子节点不为空，则在其左子树中找到最右边的节点，作为前驱节点，将当前节点的右子节点赋给前驱节点的右子节点，
+     * 然后将当前节点的左子节点赋给当前节点的右子节点，并将当前节点的左子节点设为空。对当前节点处理结束后，继续处理链表中的下一个节点，
+     * 直到所有节点都处理结束
+     * <p>
+     * 时间复杂度：O(n)
+     * 空间复杂度：O(1)
+     */
+    public void flatten5(TreeNode root) {
+        TreeNode curr = root;
+        while (curr != null) {
+            if (curr.left != null) {
+                TreeNode next = curr.left;
+                TreeNode predecessor = next;
+                while (predecessor.right != null) {
+                    predecessor = predecessor.right;
+                }
+                predecessor.right = curr.right;
+                curr.left = null;
+                curr.right = next;
+            }
+            curr = curr.right;
+        }
     }
 
     public static void main(String[] args) {
