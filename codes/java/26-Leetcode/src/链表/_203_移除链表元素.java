dc4976ffb4a753c13c6d008b15a590683a940e87
lile <1250124009@qq.com>
2025-03-10 22:51:35 +0800

    feat：移除链表元素

--- codes/java/26-Leetcode/src/链表/_203_移除链表元素.java
+++ codes/java/26-Leetcode/src/链表/_203_移除链表元素.java
@@ -109,6 +109,24 @@ public class _203_移除链表元素 {
 
         return dummyNode.next;
     }
+
+    public ListNode removeElements5(ListNode head, int val) {
+        if (head == null) {
+            return head;
+        }
+
+        // 假设 removeElements() 返回后面完整的已经去掉val节点的子链表
+        // 在当前递归层用当前节点接住后面的子链表
+        // 随后判断当前层的node是否需要被删除，如果是，就返回
+        // 也可以先判断是否需要删除当前node，但是这样条件语句会比较不好想
+        head.next = removeElements5(head.next, val);
+        if (head.val == val) {
+            return head.next;
+        }
+        return head;
+
+        // 实际上就是还原一个从尾部开始重新构建链表的过程
+    }
 }
 
 
