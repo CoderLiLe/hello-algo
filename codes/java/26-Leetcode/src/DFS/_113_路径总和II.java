ca587e61a342f4d98a720dfecabbc4aaf13cc2ee
lile <1250124009@qq.com>
2025-03-30 17:36:15 +0800

    feat：路径总和II

--- codes/java/26-Leetcode/src/DFS/_113_路径总和II.java
+++ codes/java/26-Leetcode/src/DFS/_113_路径总和II.java
@@ -3,6 +3,7 @@ package DFS;
 import common.TreeNode;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class _113_路径总和II {
@@ -26,5 +27,98 @@ public class _113_路径总和II {
             dfs(node.right, remain, nums, list);
         }
         nums.remove(nums.size() - 1);
+    }
+
+    public List<List<Integer>> pathSum1(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new LinkedList<>();
+        preorderDfs(root, targetSum, res, path);
+        return res;
+    }
+
+    private void preorderDfs(TreeNode root, int targetSum, List<List<Integer>> res, List<Integer> path) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+
+        if (root.left == null && root.right == null) {
+            if (root.val == targetSum) {
+                res.add(new ArrayList(path));
+            }
+            return;
+        }
+
+        if (root.left != null) {
+            preorderDfs(root.left, targetSum - root.val, res, path);
+            path.removeLast();
+        }
+
+        if (root.right != null) {
+            preorderDfs(root.right, targetSum - root.val, res, path);
+            path.removeLast();
+        }
+    }
+
+    public List<List<Integer>> pathSum2(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new LinkedList<>();
+        inorderDfs(root, targetSum, res, path);
+        return res;
+    }
+
+    private void inorderDfs(TreeNode root, int targetSum, List<List<Integer>> res, List<Integer> path) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+
+        if (root.left != null) {
+            inorderDfs(root.left, targetSum - root.val, res, path);
+            path.removeLast();
+        }
+
+        if (root.left == null && root.right == null) {
+            if (root.val == targetSum) {
+                res.add(new ArrayList(path));
+            }
+            return;
+        }
+
+        if (root.right != null) {
+            inorderDfs(root.right, targetSum - root.val, res, path);
+            path.removeLast();
+        }
+    }
+
+    public List<List<Integer>> pathSum3(TreeNode root, int targetSum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new LinkedList<>();
+        postorderDfs(root, targetSum, res, path);
+        return res;
+    }
+
+    private void postorderDfs(TreeNode root, int targetSum, List<List<Integer>> res, List<Integer> path) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+
+        if (root.left != null) {
+            postorderDfs(root.left, targetSum - root.val, res, path);
+            path.removeLast();
+        }
+
+        if (root.right != null) {
+            postorderDfs(root.right, targetSum - root.val, res, path);
+            path.removeLast();
+        }
+
+        if (root.left == null && root.right == null) {
+            if (root.val == targetSum) {
+                res.add(new ArrayList(path));
+            }
+            return;
+        }
     }
 }
