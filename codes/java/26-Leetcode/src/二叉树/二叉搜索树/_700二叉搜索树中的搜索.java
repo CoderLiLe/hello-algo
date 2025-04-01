85da58040a9b94e584727fb6107e516624ec8a92
lile <1250124009@qq.com>
2025-04-01 23:12:58 +0800

    feat：700二叉搜索树中的搜索

--- codes/java/26-Leetcode/src/二叉树/二叉搜索树/_700二叉搜索树中的搜索.java
+++ codes/java/26-Leetcode/src/二叉树/二叉搜索树/_700二叉搜索树中的搜索.java
@@ -3,6 +3,8 @@ package 二叉树.二叉搜索树;
 import common.TreeNode;
 import tools.Asserts;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class _700二叉搜索树中的搜索 {
@@ -58,6 +60,29 @@ public class _700二叉搜索树中的搜索 {
             }
             if (curr.right != null) {
                 stack.push(curr.right);
+            }
+        }
+        return null;
+    }
+
+    public TreeNode searchBST4(TreeNode root, int val) {
+        if (root == null || root.val == val) {
+            return root;
+        }
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node.val == val) {
+                return node;
+            }
+            if (node.left != null) {
+                queue.offer(node.left);
+            }
+            if (node.right != null) {
+                queue.offer(node.right);
             }
         }
         return null;
